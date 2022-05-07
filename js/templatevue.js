df9a1be218801fec36efe524eab9c4d73e58aefe
khksamuel <83699297+khksamuel@users.noreply.github.com>
2022-05-07 09:34:20 +0930

    desktop var to check device type

--- js/templatevue.js
+++ js/templatevue.js
@@ -2,8 +2,9 @@ const vueinst = new Vue({
     el: '#app',
     data : 
     {
-        loggedin: false,
-        baselink : "/wdc_project_2022/"
+        loggedin : false,
+        baselink : "/wdc_project_2022/",
+        desktop : true
     },
 });
 
