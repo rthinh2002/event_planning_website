edcb7ec11078cfdffd8f0852f6b7ac501fe8211e
rthinh2002 <rthinh2002@gmail.com>
2022-06-02 18:29:41 +0930

    Done with display info for the event

---
+++ public/javascripts/manageEvents.js
@@ -0,0 +1,119 @@
+function display_event_info() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var event_info = JSON.parse(this.responseText);
+            document.getElementById("eventWhat").value = event_info[0].event_name;
+            document.getElementById("eventLocation").value = event_info[0].location;
+
+            var correct_RSVP = new Date(event_info[0].RSVP);
+            correct_RSVP.setDate(correct_RSVP.getDate()+1);
+            var date = JSON.stringify(correct_RSVP);
+            // <input class="textField addMargin" type="datetime-local" placeholder="+  Add date" id="eventDate" name="eventDate" size="14">
+            document.getElementById("eventRSVP").value = date.slice(1,11);
+            document.getElementById("eventDetails").value = event_info[0].event_description;
+
+            // populate with all date;
+            for (var element in event_info) {
+                let create_input = document.createElement("input");
+                create_input.setAttribute("type", "datetime-local");
+                create_input.setAttribute("size", "14");
+                create_input.classList.add("textField");
+                create_input.classList.add("addMargin");
+
+                var d = new Date(event_info[element].event_date);
+                create_input.setAttribute("value", d.toDateTimeLocal());
+                document.getElementById("when_field").appendChild(create_input);
+            }
+        }
+    };
+    xhttp.open("POST", "/display_event_info", true);
+    xhttp.send();
+}
+
+function load_attendee() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            /*<tr>
+                <td>Who:</td>
+                <td>
+                    <input class="textField2 margin-increase" type="text" placeholder="Name" id="eventName" name="eventName" size="14">
+                </td>
+                <td>
+                    <input class="textField2 addMargin-intense setInputWidth" type="text" placeholder="Email" id="eventEmail" name="eventEmail" size="31">
+                </td>
+            </tr>*/
+            var attendee_info = JSON.parse(this.responseText);
+            for(var item in attendee_info) {
+                let create_tr = document.createElement("tr");
+                let create_td_empty = document.createElement("td");
+                // First row will have to display Who:
+                if(item == 0) {
+                    create_td_empty.innerHTML = "Who:";
+                } 
+
+                let create_td_name = document.createElement("td");
+                let create_td_email = document.createElement("td");
+                let create_input_name =document.createElement("input");
+                let create_input_email =document.createElement("input");
+
+                // add class
+                create_input_name.classList.add("textField2");
+                create_input_name.classList.add("margin-increase");
+
+                create_input_email.classList.add("textField2");
+                create_input_email.classList.add("addMargin-intense");
+                create_input_email.classList.add("setInputWidth");
+
+                // set type
+                create_input_email.setAttribute("type", "text");
+                create_input_name.setAttribute("type", "text");
+
+                // set size
+                create_input_email.setAttribute("size", "31");
+                create_input_name.setAttribute("size", "14");
+
+                // set value
+                create_input_email.setAttribute("value", attendee_info[item].email_address);
+                create_input_name.setAttribute("value", attendee_info[item].first_name);
+
+                // append
+                create_td_name.appendChild(create_input_name);
+                create_td_email.appendChild(create_input_email);
+
+                create_tr.appendChild(create_td_empty);
+                create_tr.appendChild(create_td_name);
+                create_tr.append(create_td_email);
+
+                document.getElementById("table_who").appendChild(create_tr);
+            }
+        }
+    };
+    xhttp.open("POST", "/display_attendee", true);
+    xhttp.send();
+}
+
+function start_loading()
+{
+    display_event_info();
+    load_attendee();
+}
+
+// This function is to set up the prototype of toDateTimeLocal for datetime-local format of input
+Date.prototype.toDateTimeLocal = 
+    function toDateTimeLocal() {
+        var 
+            date = this,
+            ten = function (i) {
+                return (i < 10 ? '0' : '') + i;
+            },
+            YYYY = date.getFullYear(),
+            MM = ten(date.getMonth() + 1),
+            DD = ten(date.getDate()),
+            HH = ten(date.getHours()),
+            II = ten(date.getMinutes()),
+            SS = ten(date.getSeconds())
+            ;
+            return YYYY + '-' + MM + '-' + DD + 'T' + HH + ':' + II + ':' + SS;
+    };
