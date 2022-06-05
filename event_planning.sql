346d69d7f7b03b436ecd108d59993abb89fb3981
rthinh2002 <rthinh2002@gmail.com>
2022-06-05 11:15:07 +0930

    Update database

--- event_planning.sql
+++ event_planning.sql
@@ -18,14 +18,15 @@
 --
 -- Table structure for table `attendee`
 --
-USE event_planning;
+
 DROP TABLE IF EXISTS `attendee`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `attendee` (
   `event_id` int NOT NULL,
   `user_id` int NOT NULL,
-  `attendee_response` varchar(6) DEFAULT NULL,
+  `attendee_response` varchar(6) DEFAULT 'MAYBE',
+  UNIQUE KEY `event_id_2` (`event_id`,`user_id`),
   KEY `event_id` (`event_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `attendee_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`),
@@ -39,7 +40,7 @@ CREATE TABLE `attendee` (
 
 LOCK TABLES `attendee` WRITE;
 /*!40000 ALTER TABLE `attendee` DISABLE KEYS */;
-INSERT INTO `attendee` VALUES (1,2,'YES'),(2,2,'MAYBE'),(1,1,'YES');
+INSERT INTO `attendee` VALUES (1,1,'YES'),(1,2,'YES'),(1,3,'MAYBE'),(2,2,'MAYBE');
 /*!40000 ALTER TABLE `attendee` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,7 +52,7 @@ DROP TABLE IF EXISTS `event`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `event` (
-  `event_name` varchar(20) DEFAULT NULL,
+  `event_name` varchar(100) DEFAULT NULL,
   `event_description` text,
   `creator_id` int NOT NULL,
   `event_id` int NOT NULL AUTO_INCREMENT,
@@ -70,9 +71,7 @@ CREATE TABLE `event` (
 
 LOCK TABLES `event` WRITE;
 /*!40000 ALTER TABLE `event` DISABLE KEYS */;
-INSERT INTO `event` VALUES
-('The Day of Dancing','Nothing special, just dancing around',1,1,'Adelaide Red Square','2022-01-06'),
-('Sing till Death','Just sing lol',1,2,'Botanic Garden','2022-06-15');
+INSERT INTO `event` VALUES ('The Day of Dancing Around','Nothing special, just dancing around',1,1,'Adelaide Glenelgh','2022-01-05'),('Sing till Death','Just sing lol',1,2,'Botanic Garden','2022-06-15');
 /*!40000 ALTER TABLE `event` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -99,11 +98,7 @@ CREATE TABLE `event_date` (
 
 LOCK TABLES `event_date` WRITE;
 /*!40000 ALTER TABLE `event_date` DISABLE KEYS */;
-INSERT INTO `event_date` VALUES
-('2022-01-27 15:00:00',1,_binary '\0'),
-('2022-01-28 15:00:00',1,_binary ''),
-('2022-06-22 00:00:00',2,_binary ''),
-('2022-06-23 00:00:00',2,_binary '');
+INSERT INTO `event_date` VALUES ('2022-01-27 15:00:00',1,_binary '\0'),('2022-01-28 15:00:00',1,_binary ''),('2022-06-01 00:45:00',1,_binary '\0'),('2022-06-01 11:03:00',1,_binary '\0'),('2022-06-11 11:03:00',1,_binary '\0'),('2022-06-18 11:03:00',1,_binary '\0'),('2022-06-22 00:00:00',2,_binary ''),('2022-06-23 00:00:00',2,_binary ''),('2022-06-28 23:45:00',1,_binary '\0');
 /*!40000 ALTER TABLE `event_date` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -140,11 +135,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES
-('admin', 'admin', NULL, '$argon2i$v=19$m=4096,t=3,p=1$kt+vixE37tDD5TDiUePuFg$iVnJwmPydpew6Fs2LW4CJeo52eZ/QSnBs1VLVjjn868', 'admin', 'admin', 0, NULL, NULL,_binary '',_binary '',_binary '',_binary ''),
-('Peter','Le','rthinh2002@gmail.com','$argon2i$v=19$m=4096,t=3,p=1$DtKTOQgUASMiTmkmM0znMg$CoWwbWq5yyEgEvf0v0ER2BLLIZNx6+zVP1lfAwWkCtY','peterle','admin',1,'rasdfasdfasdr-gasdf','2005-06-19',_binary '',_binary '',_binary '',_binary ''),
-('Josh','Harmon','hjosh@gmail.com','$argon2i$v=19$m=4096,t=3,p=1$2eem+yixp2eeXgYxLYT2qA$Zsg+pf+ze+3Smheg7HzSIHhJPZUYPOmjZNVFHTW3J60','joshgie','user',2,'sds-gasdf','1997-05-02',_binary '',_binary '',_binary '',_binary ''),
-('Maria','Mione','mariathegreat@gmail.com','babigurl','mariaisfabulous','user',3,'sds-ssss','2008-02-28',_binary '',_binary '',_binary '',_binary '');
+INSERT INTO `users` VALUES ('Peter','Le','rthinh2002@gmail.com','peterhandsome123','peterle','admin',1,'rasdfasdfasdr-gasdf','2005-06-19',_binary '',_binary '',_binary '',_binary ''),('Josh','NoGay','hjosh@gmail.com','$argon2i$v=19$m=4096,t=3,p=1$2eem+yixp2eeXgYxLYT2qA$Zsg+pf+ze+3Smheg7HzSIHhJPZUYPOmjZNVFHTW3J60','joshgie','user',2,'sds-gasdf','1997-05-02',_binary '',_binary '',_binary '',_binary ''),('Maria','Mione','mariathegreat@gmail.com','babigurl','mariaisfabulous','user',3,'sds-ssss','2008-02-28',_binary '',_binary '',_binary '',_binary '');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -157,4 +148,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-31 23:40:36
+-- Dump completed on 2022-06-05 11:13:43
