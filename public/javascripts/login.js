1ab61bb84514565abce085e429eddf41ab86f935
Ka Hei Samuel Kwok <a1821027@student.adelaide.edu.au>
2022-06-06 10:16:31 +0930

    removing google stuff

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,24 +1,24 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
-const GOOGLE_CLIENT_ID = '376889211664-23uvkba9h1eb2shsj4htgr6avk4jq8qp.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
+// const GOOGLE_CLIENT_ID = '376889211664-23uvkba9h1eb2shsj4htgr6avk4jq8qp.apps.googleusercontent.com';
+// const GOOGLE_CLIENT_SECRET = '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';
 
-passport.use(new GoogleStrategy({
-    clientID:     GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/google/callback",
-    passReqToCallback   : true
-  },
-  function(request, accessToken, refreshToken, profile, done) {
-      return done(err, profile);
-  }
-));
+// passport.use(new GoogleStrategy({
+//     clientID:     GOOGLE_CLIENT_ID,
+//     clientSecret: GOOGLE_CLIENT_SECRET,
+//     callbackURL: "http://localhost:3000/google/callback",
+//     passReqToCallback   : true
+//   },
+//   function(request, accessToken, refreshToken, profile, done) {
+//       return done(err, profile);
+//   }
+// ));
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+// passport.serializeUser(function(user, done) {
+//   done(null, user);
+// });
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
+// passport.deserializeUser(function(user, done) {
+//   done(null, user);
+// });
