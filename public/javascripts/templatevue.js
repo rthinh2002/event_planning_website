8b4602cc22d314560d943b3b177e54452110befa
pianoman1986 <50366173+pianoman1986@users.noreply.github.com>
2022-06-07 14:11:31 +0000

    file reorganise

--- public/javascripts/templatevue.js
+++ public/javascripts/templatevue.js
@@ -105,17 +105,4 @@ function login() {
 
 }
 
-function logout() {
 
-    let xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log('logged out');
-            window.location='/login.html';
-        }
-    };
-
-    xhttp.open("POST", "/logout");
-    xhttp.send();
-}
